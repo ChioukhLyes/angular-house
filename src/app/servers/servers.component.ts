1899bbc524989929ed64016795364e14461a2408
Lyes <lyes.chioukh-ext@eapec.fr>
2018-03-21 18:47:39 +0100

    feat(servers): enhacing ngIf with else condition

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   allowNewServer = true;
   serverCreationStatus = 'No server was created.';
   serverName = 'Test';
+  serverCreated = false;
 
   constructor() {
   }
@@ -23,6 +24,7 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
   }
 
