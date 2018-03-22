7fa32b9b5bafb744f3d015f826dc96cac4f19f08
Lyes <lyes.chioukh-ext@eapec.fr>
2018-03-22 09:23:15 +0100

    feat(servers): using *ngFor inside component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -16,6 +16,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created.';
   serverName = 'Test';
   serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
 
   constructor() {
   }
@@ -25,6 +26,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created. Name is ' + this.serverName;
   }
 
